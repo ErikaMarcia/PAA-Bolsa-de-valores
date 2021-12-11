5276eb2bf6daf4e5fbac6c0bce554f8bcf49d351
Erika Marcia <frwk.erika@unidas.com.br>
2021-12-11 18:20:44 -0300

    feat - montar portifolio

--- sistema.js
+++ sistema.js
@@ -45,27 +45,6 @@ function desvioPadrao(ativo) {
   return Math.sqrt(variacao / precos.length);
 }
 
-function teste(ativo) {
-  let precos = montarArrayAtivo(ativo);
-  let mediaPrecos = retornoEsperadoDeUmAtivo(ativo);
-  let desvio = [];
-  for (let i = 0; i < precos.length; i++) {
-    desvio.push(precos[i] - mediaPrecos);
-  }
-
-  let variancia = 0;
-
-  for (let i = 0; i < desvio.length; i++) {
-    variancia += desvio[i] ^ 2;
-  }
-
-  variancia = variancia / mediaPrecos;
-
-  let desvioPadrao = Math.sqrt(variancia);
-
-  console.log(desvioPadrao);
-}
-
 function riscoNormalizado(ativo) {
   return desvioPadrao(ativo) / retornoEsperadoDeUmAtivo(ativo);
 }
@@ -89,3 +68,136 @@ function montarArrayAtivo(nomeAtivo) {
   }
   return array;
 }
+
+function montarArrayRiscos() {
+  //Buscando os riscos de cada ativo
+  return [
+    {
+      ativo: "alzr",
+      risco: riscoAtivo(montarArrayAtivo("alzr")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "bpff",
+      risco: riscoAtivo(montarArrayAtivo("bpff")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "brcr",
+      risco: riscoAtivo(montarArrayAtivo("brcr")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "hgbs",
+      risco: riscoAtivo(montarArrayAtivo("hgbs")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "hglg",
+      risco: riscoAtivo(montarArrayAtivo("hglg")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "hgre",
+      risco: riscoAtivo(montarArrayAtivo("hgre")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "jsre",
+      risco: riscoAtivo(montarArrayAtivo("jsre")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "kncr",
+      risco: riscoAtivo(montarArrayAtivo("kncr")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "knip",
+      risco: riscoAtivo(montarArrayAtivo("knip")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+    {
+      ativo: "xpml",
+      risco: riscoAtivo(montarArrayAtivo("xpml")),
+      porcentagem: Math.random() * (10 - 0) + 0,
+    },
+  ];
+}
+
+function montarPortifolio() {
+  let riscos = montarArrayRiscos();
+  let capacidade = 15;
+  let melhor_risco = 0,
+    melhor_peso = 0;
+  let num_items = riscos.length;
+  let num_solucoes = 1024 * 1024 * 32 * 4;
+  let solucao = 0;
+
+  for (let i = 0; i < num_solucoes; i++) {
+    let valor_mochila = 0,
+      peso_mochila = 0;
+
+    // Verificar a solucao.
+    for (let j = 0; j < num_items; j++) {
+      if (i & (1 << j)) {
+        valor_mochila += riscos[j].risco;
+        peso_mochila += riscos[j].porcentagem;
+      }
+    }
+
+    if (peso_mochila <= capacidade) {
+      if (valor_mochila > melhor_risco) {
+        solucao = i;
+        melhor_peso = peso_mochila;
+        melhor_risco = valor_mochila;
+      }
+    }
+  }
+
+  console.log("peso da mochila: ", melhor_peso.toFixed(2));
+  console.log("risco da mochila: ", (melhor_risco / 100).toFixed(2), "%");
+}
+
+montarPortifolio();
+// //definindo capacidade maxima do portifolio
+// const capacidade = 10;
+// //definindo variaveis
+// let contadorPortifolio = 0;
+// let ativos = [];
+// let max;
+// let valorPortifolio;
+
+// //funcao para remover item do portifolio para fazer proxima verificacao
+// var removeByAttr = function (arr, attr, value) {
+//   var i = arr.length;
+//   while (i--) {
+//     if (
+//       arr[i] &&
+//       arr[i].hasOwnProperty(attr) &&
+//       arguments.length > 2 &&
+//       arr[i][attr] === value
+//     ) {
+//       arr.splice(i, 1);
+//     }
+//   }
+// };
+// do {
+//   //encontra o item com maior valor
+//   max = riscos.reduce(function (anterior, atual) {
+//     return anterior.risco > atual.risco ? anterior : atual;
+//   });
+//   //se ainda tiver espaco na mochila ele inclui o elemento com peso maximo
+//   if (max.peso + contadorPortifolio <= capacidade) {
+//     ativos.push(max);
+//     contadorPortifolio += max.peso; //ele aumenta o valor do contador da mochila
+//     valorPortifolio += max.risco; //ele aumenta o valor da mochila
+//     removeByAttr(riscos, "id", max.id); //remove o item com maior valor da relacao inicial de itens
+//   } else {
+//     //caso o item com peso maximo nao couber na mochila ele apenas remove esse item da relacao inicial para a proxima verificacao
+//     removeByAttr(riscos, "id", max.id);
+//   }
+// } while (capacidade > contadorPortifolio && riscos.length > 0); //roda o loop ate que a capacidade esgote ou ate que a relacao inicial de itens esteja vazia
+
+// console.log(ativos);
+// console.log(valorPortifolio);
